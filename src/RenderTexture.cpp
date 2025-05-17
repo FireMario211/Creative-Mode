d92065170d678693bbd34427ec873cb483ed4053
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-05-17 19:31:40 -0400

    Try fix

--- src/RenderTexture.cpp
+++ src/RenderTexture.cpp
@@ -21,6 +21,7 @@ RenderTexture::RenderTexture(unsigned int width, unsigned int height, GLint text
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
 	glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_oldFBO);
+	glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_oldRBO);
 
     glGenFramebuffers(1, &m_fbo);
     glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
@@ -38,6 +39,7 @@ RenderTexture::RenderTexture(unsigned int width, unsigned int height, GLint text
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_texture, 0);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, m_oldFBO);
+	glBindRenderbuffer(GL_RENDERBUFFER, m_oldRBO);
 }
 
 RenderTexture::RenderTexture(RenderTexture&& other) {
